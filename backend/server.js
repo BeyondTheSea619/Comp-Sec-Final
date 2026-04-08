6258ccf828b6b8b5a89d78bca6f75ac1f140c5d9
Nayeon Cho <ncho2769@conestogac.on.ca>
2026-04-08 18:22:15 -0400

    added upload route, setting multer, and create folder for use upload file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 // middleware
 const app = express();
@@ -7,6 +10,38 @@ const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// making upload folder
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+// configure file storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    // Save file with timestamp to avoid duplicate names
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+// Restrict file type and file size
+const upload = multer({
+  storage,
+  limits: { fileSize: 1000000 }, // 1 MB limit
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only JPG, JPEG, and PNG files are allowed"));
+    }
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Backend server is running");
@@ -56,6 +91,18 @@ app.get("/api/search", (req, res) => {
   const results = items.filter((item) => item.name.toLowerCase().includes(q));
 
   res.json(results);
+});
+
+// upload route
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  res.json({
+    message: "File uploaded successfully",
+    filename: req.file.filename,
+  });
 });
 
 app.listen(PORT, () => {
