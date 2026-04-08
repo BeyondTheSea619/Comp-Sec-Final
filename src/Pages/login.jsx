0ce6f5b5cabaa30682482785e1508d5b8238adfa
Nayeon Cho <ncho2769@conestogac.on.ca>
2026-04-08 17:46:47 -0400

    added comments

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -4,25 +4,33 @@ function Login() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // new: state to display login result message
   const [message, setMessage] = useState("");
 
+  // new: handle form submission and call backend API
   const handleSubmit = async (e) => {
+    // prevent page reload
     e.preventDefault();
     try {
+      // new: send POST request ot express backend login API
       const res = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
+        // sending email and password to server for authentication
         body: JSON.stringify({ email, password }),
       });
-
+      // parse response JSON
       const data = await res.json();
-
+      // check if login success
       if (res.ok) {
+        // show success message and user role
         setMessage(`${data.message} (${data.role})`);
+        // log token can be used later for auth
         console.log("Token:", data.token);
       } else {
+        // show error message from server
         setMessage(data.message);
       }
     } catch (error) {
@@ -33,6 +41,7 @@ function Login() {
 
   return (
     <div>
+      {/* updated action -> onSubmit */}
       <form onSubmit={handleSubmit}>
         <div className="formgroup">
           <label className="formlabel">Name:</label>
@@ -72,8 +81,10 @@ function Login() {
             }}
           />
         </div>
+        {/* add submit button */}
+        <button type="submit">Login</button>
       </form>
-
+      {/* display login result */}
       {message && <p>{message}</p>}
     </div>
   );
