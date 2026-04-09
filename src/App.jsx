1892205d62b3179d19d13c7707cebcda8cc92739
Joey <Jchanthasy3577@conestogac.on.ca>
2026-04-09 15:49:02 -0400

    Added admin button when logged into admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import Login from "./Pages/login";
 import Home from "./Pages/home";
 import Search from "./Pages/Search";
+import Admin from "./Pages/admin";
 import FileUpload from "./Pages/upload";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const userRole = localStorage.getItem("userRole");
   return (
     <BrowserRouter>
       <div>
@@ -23,6 +25,11 @@ function App() {
           <Link className="" to="/Pages/upload">
             Upload
           </Link>
+          {userRole === "admin" && (
+            <Link className="text-danger fw-bold" to="/Pages/admin">
+              Admin
+            </Link>
+          )}
         </div>
 
         <Routes>
@@ -30,6 +37,7 @@ function App() {
           <Route path="/Pages/login" element={<Login />}></Route>
           <Route path="/Pages/search" element={<Search />}></Route>
           <Route path="/Pages/upload" element={<FileUpload />}></Route>
+          <Route path="/Pages/admin" element={<Admin />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
